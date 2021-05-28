10622b2755bcc8e51848dc76a9fc3dd1cc9ba4c5
Bhavuk <sahilthakral7908@gmail.com>
2021-05-28 15:18:25 +0530

    render dynamic data part2(episode 19)

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::latest()->get();
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+    Public function show($id)
+    {
+        $article = Article::find($id);
+
+        return view('articles.show',['article'=>$article]);
+    }
+
+    public function create()
+    {
+
+
+    }
+
+    public function store()
+    {
+
+
+    }
+
+    public function edit()
+    {
+
+
+    }
+
+    public function update()
+    {
+
+
+    }
+
+    public function destroy()
+    {
+
+
+    }
+
+
+}
